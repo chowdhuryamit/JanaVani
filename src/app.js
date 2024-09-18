ac47b3cb689d7c90d337e048211764688d586d24
Amit chowdhury <camit8546@gmail.com>
2024-09-18 20:15:18 +0530

    view engine setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 
 const app=express();
 
@@ -12,6 +16,16 @@ app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'./views'));
+
+
+app.get('/',async(req,res)=>{
+    return res.render('signup');
+})
 
 
 export {app};
---
+++ src/views/partials/script.ejs
@@ -0,0 +1,8 @@
+<script>
+    const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuToggle.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+</script>
